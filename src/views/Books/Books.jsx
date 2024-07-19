547e64326512fd52fb46bd1b417055ac43bf683c
More-Pe <peraltamorena.va@gmail.com>
2024-07-19 13:11:33 +0200

    refactor: book view

--- src/views/Books/Books.jsx
+++ src/views/Books/Books.jsx
@@ -3,33 +3,31 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 export const Books = () => {
-
-	const [books, setBooks] = useState([])
+	const [books, setBooks] = useState([]); //cuidado, esto es solo para arrays
 
 	useEffect(() => {
-		console.log("UseEffect");
-		fetch("http://localhost:4000/books")
-		.then(res=> {
-            return res.json();
-		})
-		.then(res=> {
-			setBooks(res.data);
-			console.log(res);
-		})
-		.catch(e => {
-		    console.log(res);
-		});
-	},
-[]
-);
+		console.log('UseEffect');
+		fetch('http://localhost:4000/books')
+			.then((res) => {
+				return res.json();
+			})
+			.then((res) => {
+				setBooks(res.data);
+				console.log(res);
+			})
+			.catch((e) => {
+				console.log(res);
+			});
+	}, []);
 
 	return (
 		<>
-		{
-			books.map( (book) => {
-				return book.title;
-			})
-		}
+			{books.map((book) => (
+				<div key={book.id} className='card'>
+					<h1> {book.title} </h1>
+					<p> {book.description}</p>
+				</div> //está todo envuelto en un div porque puede renderizar un solo elemento
+			))}
 		</>
-);
+	);
 };
